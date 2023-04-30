c93c7cf0f3cd34105410039e1573ebfac0bbbb03
Luka Bonheure <Luka.Bonheure@ulb.be>
2023-04-30 14:06:45 +0200

    comments in AppController

--- src/Controllers/AppController.py
+++ src/Controllers/AppController.py
@@ -4,7 +4,14 @@ from Controllers.ResultTabControllers.ResultTabController import ResultTabContro
 from Models.PrometheeGamma import PrometheeGamma
 
 class AppController(AppView.ViewListener, ResultTabController.Listener):
+    """
+    The main controller of the application.
+    It allows to start and stop the application, but also to make the link between its different parts.
+    """
     def __init__(self) -> None:
+        """
+        Constructor
+        """
         self.appView = AppView()
         self.appView.setListener(self)
         self.alreadyCompute = False
@@ -21,18 +28,27 @@ class AppController(AppView.ViewListener, ResultTabController.Listener):
         self.appView.mainloop()
 
 
-    def showDataTabView(self, master):
+    def showDataTabView(self, master) -> None:
+        """
+        Show the data tab
+        """
         self.dataTabController = DataTabController(master=master)
         self.dataTabController.showView()
 
 
-    def showResultTabView(self, master):
+    def showResultTabView(self, master) -> None:
+        """
+        Show the result tab
+        """
         self.resultTabController = ResultTabController(master=master)
         self.resultTabController.setListener(self)
         self.resultTabController.showView()
 
 
-    def computeResults(self):
+    def computeResults(self) -> None:
+        """
+        Compute the result of the Promethee Gamma method
+        """
         dataTabModel = self.dataTabController.getModel()
         resultTabModel = self.resultTabController.getModel()
         self.prometheeGamma.setDataTabModel(dataTabModel)
@@ -42,7 +58,11 @@ class AppController(AppView.ViewListener, ResultTabController.Listener):
         self.resultTabController.refreshResultsVisualisation()
 
 
-    def changeOnTiAndTj(self):
+    def changeOnTiAndTj(self) -> None:
+        """
+        Recompute the needed results in case of change on Ti and Tj.
+        This method must be called if there is a simultaneous change of the thresholds Ti and Tj.
+        """
         if self.alreadyCompute:
             self.prometheeGamma.build_matrix_I()
             self.prometheeGamma.build_matrix_J()
@@ -50,30 +70,48 @@ class AppController(AppView.ViewListener, ResultTabController.Listener):
             self.resultTabController.refreshResultsVisualisation()
 
 
-    def changeOnTi(self):
+    def changeOnTi(self) -> None:
+        """
+        Recompute the needed results in case of change on Ti.
+        This method must be called if there is a change of the threshold Ti and not on Tj.
+        """
         if self.alreadyCompute:
             self.prometheeGamma.build_matrix_I()
             self.prometheeGamma.computeResults()
             self.resultTabController.refreshResultsVisualisation()
 
 
-    def changeOnTj(self):
+    def changeOnTj(self) -> None:
+        """
+        Recompute the needed results in case of change on Tj.
+        This method must be called if there is a change of the threshold Tj and not on Ti.
+        """
         if self.alreadyCompute:
             self.prometheeGamma.build_matrix_J()
             self.computeResults()
             self.resultTabController.refreshResultsVisualisation()
 
 
-    def changeOnPf(self):
+    def changeOnPf(self) -> None:
+        """
+        Recompute the needed results in case of change on Pf.
+        This method must be called if there is a change of the parameter Pf.
+        """
         if self.alreadyCompute:
             self.prometheeGamma.build_matrix_P()
             self.computeResults()
             self.resultTabController.refreshResultsVisualisation()
 
 
-    def getPrometheeGammaModel(self):
+    def getPrometheeGammaModel(self) -> PrometheeGamma:
+        """
+        Return the current used model for the Promethee Gamma method
+        """
         return self.prometheeGamma
     
 
     def getDataTabModel(self):
+        """
+        Return the current data tab model
+        """
         return self.dataTabController.getModel()
